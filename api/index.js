237d18540380aff7797b2d421b8271dfaf566bde
ParkSeoHai <anhhai282003@gmail.com>
2024-10-26 17:05:34 +0700

    add mongo

--- api/index.js
+++ api/index.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose');
+const Blog = require('./model/blog');
 const express = require("express");
+
+mongoose.connect("mongodb+srv://parkseohai:imxMknCsDEdQ5KfQ@cluster0.ezm32.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/addBlog", async (req, res, next) => {
+    // Create a new blog post object
+    const article = new Blog({
+    title: 'Awesome Post!',
+    slug: 'awesome-post',
+    published: true,
+    content: 'This is the best post ever',
+    tags: ['featured', 'announcement'],
+  });
+  // Insert the article in our MongoDB database
+  const result = await article.save();
+  res.json(result);
+})
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
---
+++ api/model/blog.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const blogSchema = new Schema({
+  title: String,
+  slug: String,
+  published: Boolean,
+  author: String,
+  content: String,
+  tags: [String],
+  createdAt: Date,
+  updatedAt: Date,
+  comments: [{
+    user: String,
+    content: String,
+    votes: Number
+  }]
+});
+
+const Blog = model('Blog', blogSchema);
+module.exports = Blog;
