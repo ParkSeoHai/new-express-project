61cbc9f810bb8e8368b67448d6d3d914bf920af4
ParkSeoHai <anhhai282003@gmail.com>
2024-10-27 12:47:17 +0700

    add post

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 const Blog = require('./model/blog');
 const express = require("express");
+const cors = require('cors');
 
 mongoose.connect("mongodb+srv://parkseohai:imxMknCsDEdQ5KfQ@cluster0.ezm32.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 const app = express();
+
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
@@ -20,6 +24,16 @@ app.get("/addBlog", async (req, res, next) => {
   // Insert the article in our MongoDB database
   const result = await article.save();
   res.json(result);
+})
+
+app.post('/blog', async (req, res, next) => {
+  const newBlog = await Blog.create({
+    ...req.body
+  });
+
+  if (!newBlog) return res.status(500).json(`dd blog failed: ${req.body}`)
+
+  return res.status(201).json(newBlog)
 })
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
